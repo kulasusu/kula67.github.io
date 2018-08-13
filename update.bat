bd00ac78802f3ade5bebc0114bc6f44593236249
kulasusu <38985525+kulasusu@users.noreply.github.com>
2018-08-13 18:45:50 +0700

    Update update.bat

--- update.bat
+++ update.bat
@@ -1,5 +1,5 @@
 <<<<<<< HEAD
 bash -c "cd D:/IOS/kula67.github.io; ./update.sh;"
 =======
-bash -c "cd den72.appmobiles.ml; ./update.sh;"
+bash -c "cd kula67.lake.gq; ./update.sh;"
 >>>>>>> 4f1f40b2a22961f48e662dcc84667aae1acdce79
